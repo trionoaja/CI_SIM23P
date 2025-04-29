af1f98a92afce0d32cb86504dd060c9149e19fa1
trionoaja <trionoaja@gmail.com>
2025-04-29 14:26:07 +0700

    update model

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -27,10 +27,12 @@
   <div class="card">
     <div class="card-body login-card-body">
       <p class="login-box-msg">Sign in to start your session</p>
-
-      <form action="../../index3.html" method="post">
+      <?php if ($this->session->flashdata('error')): ?>
+        <p style="color:red;"><?= $this->session->flashdata('error'); ?> </p>
+        <?php endif; ?>
+      <form action="<?= site_url('auth/process_login')?>" method="post">
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
+          <input type="text" name="username" class="form-control" placeholder="Email">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -38,7 +40,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
+          <input type="password" name="password" class="form-control" placeholder="Password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
